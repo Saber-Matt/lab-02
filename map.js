5da84e233b3e89c0e52e0163b352e789f0ff3d59
Matthew Saber <mattsaber21@yahoo.com>
2021-06-09 10:22:49 -0700

    green map

--- map.js
+++ map.js
@@ -1,7 +1,8 @@
 export function map(arr, callback) {
   const newArr = [];
+  // eslint-disable-next-line keyword-spacing
   for (let i = 0; i < arr.length; i++) {
-    const newItem = callback(item);
+    const newItem = callback(arr[i]);
     newArr[i] = newItem;
   }
 
--- map.test.js
+++ map.test.js
@@ -3,8 +3,11 @@ import { map } from './map.js';
 describe(map, () => {
   it('generate new arr like .map does', () => {
     const arr = [5, 7, 23, 9];
-    arrMap = map(arr, () => { });
+    const arrMap = map(arr, (item) => {
+      return -item;
 
+    });
 
+    expect(arrMap).toEqual([-5, -7, -23, -9]);
   });
 });
